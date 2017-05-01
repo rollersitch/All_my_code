6ed4658f26aa5e63fa2347376e7e2b3ae470bb5d
Rollersitch <dany88vai@gmail.com>
2017-05-01 17:30:28 +0200

    Auto Git Update 2017_05_01

---
+++ Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Person {
+        public class Person {
+        private  name;
+       
+
+        public static void Main() {
+            var p = new Person(name = "Daniele");
+            Console.WriteLine(p.name);
+        }        
+    }
+}
+
+
