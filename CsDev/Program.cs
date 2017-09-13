964ba36b10d6b5b79b57e42a335643347b229106
Daniele Pipitone <dany88vai@gmail.com>
2017-09-13 17:30:30 +0200

    Auto Git Update 2017_09_13

---
+++ CsDev/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CsDev
+{
+    public delegate void MyGenericDelegate<T>(T arg);
+
+    public class Program
+    {
+        static void Main(string[] args) {
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+
+            strTarget("Some string data");
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+
+            intTarget(9);
+        }
+
+        public static void StringTarget(string arg) {
+            Console.WriteLine($"arg in uppercase: {arg.ToUpper()}");
+        }
+        public static void IntTarget(int arg) {
+            Console.WriteLine($"++ arg is : {++arg}");
+        }
+    }
+}
